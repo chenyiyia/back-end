76b761670f7b2c54e7992e793f6deed121913630
chenyiyi <1609528845@qq.com>
2023-11-14 21:19:15 +0800

    陈一一 feat:新增员工功能开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -55,4 +61,30 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+    @Override
+
+    public void save(EmployeeDTO employeeDTO){
+        //将data transfer object转为object(传输对象的数据比被持久化对象的数据少，因为持久化对象的某些数据不是前端传来的)
+
+        //1.声明持久化对象
+        Employee employee=new Employee();
+        //2.将传输对象的全部属性数据复制给持久化对象
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //3.设置持久化对象的剩余属性
+        //3.1设置状态
+        employee.setStatus(StatusConstant.ENABLE);
+        //3.2设置密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        //3.3设置创建时间
+        employee.setCreateTime(LocalDateTime.now());
+        //3.4设置修改时间
+        employee.setUpdateTime(LocalDateTime.now());
+        //3.5设置当前记录创建人的id和修改人的id
+        // TODO 后期改为当前登录用户的id
+        Long currentId=BaseContext.getCurrentId();
+        employee.setCreateUser(currentId);
+        employee.setUpdateUser(currentId);
+
+        employeeMapper.insert(employee);
+    }
 }
